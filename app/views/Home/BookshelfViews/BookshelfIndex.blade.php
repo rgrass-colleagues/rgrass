6937e402baf7cce148f83ec691523ebdbabd8e93
a81118bobo <81118bobo@163.com>
2015-08-10 18:27:53 +0800

    2015-08-10

--- app/views/Home/BookshelfViews/BookshelfIndex.blade.php
+++ app/views/Home/BookshelfViews/BookshelfIndex.blade.php
@@ -1,1 +1,17 @@
-123
+@extends('layouts.home_master')
+@section('title')
+同人坊--燃草同人社区
+@stop
+@section('content')
+@include('layouts.header')
+@include('layouts.nav')
+<div id="user_section">
+    @include('layouts.userLeft')
+
+
+    <div class="user_section_right">
+        
+    </div>
+</div>
+@include('layouts.footer')
+@stop
